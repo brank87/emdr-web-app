cffa05f6738aa614968ad1c4bea93750568a3f7f
brank87 <brank87@gmail.com>
2023-01-30 19:43:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
-const ball = document.getElementById("ball");
+const dot = document.getElementById("dot");
+const container = document.getElementById("container");
 const speedInput = document.getElementById("speed");
-const sizeInput = document.getElementById("size");
-const orientationRadios = document.getElementsByName("orientation");
+const leftSound = document.getElementById("left-sound");
+const rightSound = document.getElementById("right-sound");
 
-let x = 0;
-let y = 0;
-let speed = 100;
-let size = 50;
-let orientation = "horizontal";
-
-for (const radio of orientationRadios) {
-  radio.addEventListener("change", (event) => {
-    orientation = event.target.value;
-  });
-}
+let speed = 5;
+let direction = 1;
 
 speedInput.addEventListener("input", (event) => {
   speed = event.target.value;
 });
 
-sizeInput.addEventListener("input", (event) => {
-  size = event.target.value;
-  ball.style.width = size + "px";
-  ball.style.height = size + "px";
-  ball.style.borderRadius = size / 2 + "px";
-});
-
 setInterval(() => {
-  if (orientation === "horizontal") {
-    x += 10;
-    ball.style.left = x + "px";
-    if (x >= window.innerWidth - size) {
-      x = 0;
-    }
-  } else if (orientation === "vertical") {
-    y += 10;
-    ball.style.top = y + "px";
-    if (y >= window.innerHeight - size) {
-      y = 0;
+  const left = parseInt(getComputedStyle(dot).left);
+  const containerWidth = parseInt(getComputedStyle(container).width);
+  const dotWidth = parseInt(getComputedStyle(dot).width);
+
+  if (left + dotWidth >= containerWidth || left <= 0) {
+    direction *= -1;
+    if (direction === 1) {
+      rightSound.currentTime = 0;
+      rightSound.play();
+    } else {
+      leftSound.currentTime = 0;
+      leftSound.play();
     }
   }
-}, speed);
+
+  dot.style.left = `${left + speed * direction}px`;
+}, 50);
